b471f89e74574e56a6420ff89641f81d328150f8
Leandro Martin, Balado Vidal <lm.baladovidal@gmail.com>
2022-09-14 17:26:02 -0300

    importacion de modulos

--- 02-introJs/src/index.js
+++ 02-introJs/src/index.js
@@ -1,20 +1,17 @@
-const personajes = ['Goku','Vegeta','Trunks'];
-const [,p2,p3] = personajes;
+import heroes from './data/heroes'
+console.log(heroes);
 
-console.log(p2);
-console.log(p3);
-
-const retornaArreglo = ()=>{
-    return ['ABC',123];
+const getHeroesById = (id)=>{
+    return heroes.find((heroe) => heroe.id === id);
 }
 
-const [letras,numeros] = retornaArreglo();
-console.log(letras,numeros);
-
-const usesState = (valor)=>{
-    return [valor,()=>{console.log('Hola Mundo')}];
+const getHeroesByOwner = (owner)=>{
+    return heroes.filter((heroe) => heroe.owner === owner);
 }
 
-const [nombre,setNombre] = usesState('Goku');
-console.log(nombre);
-setNombre();
+console.log(getHeroesById(2));
+console.log(getHeroesByOwner('DC'));
+let arr = [0,1,2,3];
+arr[10] = 10;
+console.log(arr);
+console.log(arr[9]);
