9e740aa5e456c493796546e5119525a086b01956
Leandro Martín Balado Vidal <lm.baladovidal@gmail.com>
2022-09-18 23:03:31 -0300

    props

--- 03-PrimerosPasosReact/counter-app-vite/src/FirstApp.jsx
+++ 03-PrimerosPasosReact/counter-app-vite/src/FirstApp.jsx
@@ -1,3 +1,4 @@
+import PropTypes from 'prop-types'
 const newMessage = 'Leandro';
 
 function getObj(){
@@ -5,12 +6,26 @@ function getObj(){
             Apellido:"Balado Vidal"}
 }
 
-export const FirstApp = () => {
+export const FirstApp = ({title, subtitle,counter}) => {
+    console.log(title)
   return (
     <>
-        <h1>Hola{newMessage}</h1>
+        {/* <h1>Hola{newMessage}</h1> */}
+        <h1>{title}</h1>
+        <h4>{counter +1}</h4>
         <code>{JSON.stringify(getObj())}</code>
-        <p>Soy Un Subtitulo</p>
+        <p>{subtitle}</p>
     </>
   )
+}
+
+FirstApp.propTypes = {
+    title : PropTypes.string.isRequired,
+    subtitle : PropTypes.string,
+    counter : PropTypes.number
+};
+
+FirstApp.defaultProps = {
+    title : "No Hay titulo",
+    subtitle : "No Hay subtitulo"
 }
--- 03-PrimerosPasosReact/counter-app-vite/src/main.jsx
+++ 03-PrimerosPasosReact/counter-app-vite/src/main.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import './styles.css'
 import { FirstApp } from './FirstApp';
 import {HelloWorld} from './HelloWorld'
 
@@ -7,6 +8,6 @@ import {HelloWorld} from './HelloWorld'
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <HelloWorld />
-        <FirstApp/>
+        <FirstApp title={"Hola, soy Goku!"} subtitle='Soy un sexy subtitulo' counter={1}/>
     </React.StrictMode>
 );
