b7e7b733162f30474e397f4dc7d74b7e306069be
Leandro Martin, Balado Vidal <lm.baladovidal@gmail.com>
2022-09-20 17:25:14 -0300

    Finalizando intro

--- 03-PrimerosPasosReact/counter-app-vite/src/main.jsx
+++ 03-PrimerosPasosReact/counter-app-vite/src/main.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import './styles.css'
 import { FirstApp } from './FirstApp';
-import {HelloWorld} from './HelloWorld'
+import {HelloWorld} from './HelloWorld';
+import {CounterApp} from './CounterApp';
 
 
 ReactDOM.createRoot(document.getElementById('root')).render(
     <React.StrictMode>
         <HelloWorld />
-        <FirstApp title={"Hola, soy Goku!"} subtitle='Soy un sexy subtitulo' counter={1}/>
+        <CounterApp value={22}/>
+        {/* <FirstApp title={"Hola, soy Goku!"} subtitle='Soy un sexy subtitulo' counter={1}/> */}
     </React.StrictMode>
 );
