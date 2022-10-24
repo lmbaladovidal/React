e103578e109f12128848ad3b35d9d1f16915ac21
Leandro Martin, Balado Vidal <lm.baladovidal@gmail.com>
2022-10-24 16:11:18 -0300

    aplicando useRef

---
+++ 05-hooks-app/hook-app/src/04-useRef/FocusScreen.jsx
@@ -0,0 +1,24 @@
+import React, { useRef } from 'react'
+
+export const FocusScreen = () => {
+    const inputRef = useRef();
+    const onClick = ()=>{
+        inputRef.current.select();
+    }
+  return (
+    <>
+        <h1>Focus Screen</h1>
+        <input 
+            ref={ inputRef }
+            className="form-control"
+            type='text'
+            placeholder='Ingrese su nombre'></input>
+        <hr/>
+        <button 
+            className='btn btn-primary mt-2' 
+            onClick={ onClick }>
+            Set Focus
+        </button>
+    </>
+  )
+}
--- 05-hooks-app/hook-app/src/HooksApp.jsx
+++ 05-hooks-app/hook-app/src/HooksApp.jsx
@@ -4,16 +4,18 @@ import { CounterApp } from './01-useState/CounterApp'
 import { CounterWithCustomHook } from './01-useState/CounterWithCustomHook'
 import { FormWithCustomHook } from './02-useEfect/FormWithCustomHook'
 import { SimpleForm } from './02-useEfect/SimpleForm'
+import { FocusScreen } from './04-useRef/FocusScreen'
 
 export const HooksApp = () => {
   return (
     <>
         <h1>HookApp</h1>
         <hr/>
-        <CounterApp/>
+{/*         <CounterApp/>
         <CounterWithCustomHook/>
         <SimpleForm/>
-        <FormWithCustomHook/>
+        <FormWithCustomHook/> */}
+        <FocusScreen/>
     </>
   )
 }
