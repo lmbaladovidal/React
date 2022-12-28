13ea45f49ac19ffae616bc4489831c9864eda6a6
Leandro Martin, Balado Vidal <lm.baladovidal@gmail.com>
2022-12-28 16:54:55 -0300

    se agregaron los test suites para el componente TodoItem

--- 05-hooks-app/test/08-UseReducer/TodoItem.test.jsx
+++ 05-hooks-app/test/08-UseReducer/TodoItem.test.jsx
@@ -1,4 +1,4 @@
-import { render,screen } from "@testing-library/react";
+import { render,screen,fireEvent } from "@testing-library/react";
 import { TodoItem } from "../../src/08-useReducer/TodoItem";
 
 describe('Purebas en <TodoItem/>', () => { 
@@ -7,6 +7,12 @@ describe('Purebas en <TodoItem/>', () => {
         id:1,
         description:'piedra del alma',
         done:false
+    };
+    
+    const todoDone = {
+        id:1,
+        description:'piedra del alma',
+        done:true
     };
 
     const onRemoveTodoMock = jest.fn();
@@ -21,8 +27,7 @@ describe('Purebas en <TodoItem/>', () => {
                 todo={todo} 
                 onToggleTodo={onToggleTodoMock} 
                 onRemoveTodo={onRemoveTodoMock}
-
-                />
+            />
         );
         const liElement = screen.getByRole('listitem');
         const spanElement = screen.getByLabelText('span');
@@ -30,5 +35,57 @@ describe('Purebas en <TodoItem/>', () => {
         expect(spanElement.className).toBe('align-self-center ')
 
      })
+
+     test('should show the complete toDo', () => { 
+        
+        render(
+            <TodoItem 
+                todo={todoDone} 
+                onToggleTodo={onToggleTodoMock} 
+                onRemoveTodo={onRemoveTodoMock}
+            />
+        );
+        const spanElement = screen.getByLabelText('span');
+
+        expect(spanElement.className).toBe('align-self-center text-decoration-line-through')
+
+     })
+
+     test('span should call toggleTodo on Click Event', () => { 
+
+        render(
+            <TodoItem 
+                todo={todo} 
+                onToggleTodo={onToggleTodoMock} 
+                onRemoveTodo={onRemoveTodoMock}
+            />
+        );
+
+        const spanElement = screen.getByLabelText('span');
+        fireEvent.click(spanElement);
+        expect(onToggleTodoMock).toHaveBeenCalledWith(todo.id);
+
+
+      })
+
+      
+     test('button should call remoteTddo on Click Event', () => { 
+
+        render(
+            <TodoItem 
+                todo={todo} 
+                onToggleTodo={onToggleTodoMock} 
+                onRemoveTodo={onRemoveTodoMock}
+            />
+        );
+
+        const buttonElement = screen.getByRole('button',{name:'Borrar'});
+        fireEvent.click(buttonElement);
+        expect(onRemoveTodoMock).toHaveBeenCalledWith(todo);
+
+
+      })
+
+     
 
  })
