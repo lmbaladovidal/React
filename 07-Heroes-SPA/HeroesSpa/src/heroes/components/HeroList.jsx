1840bb49b55e6cbea4f98819e865defd6b25b858
Leandro Martín Balado Vidal <lm.baladovidal@gmail.com>
2023-02-06 22:40:42 -0300

    Se creo el componente <HeroCard/> y de le dieron estilos

--- 07-Heroes-SPA/HeroesSpa/src/heroes/components/HeroList.jsx
+++ 07-Heroes-SPA/HeroesSpa/src/heroes/components/HeroList.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import { HeroCard } from './';
 import { getHeroesByPublisher } from '../helpers'
 
 export const HeroList = ({publisher}) => {
@@ -10,9 +10,10 @@ export const HeroList = ({publisher}) => {
       <div className='row rows-cols-1 row-cols-md-3 g-4"'>
         {
           heroes.map( hero =>(
-            <li key= {hero.id}>
-              {hero.superhero}
-            </li>
+            <HeroCard 
+              key={hero.id}
+              {...hero}
+            />
           ))
         }
       </div>
