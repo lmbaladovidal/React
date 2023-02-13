33bc54c6c0a18d1295c1171e1d81f8b382b4d52c
Leandro Martin, Balado Vidal <lm.baladovidal@gmail.com>
2023-02-13 17:22:10 -0300

    Se finalizo la implementacion de los testSuits en <PublicRoute/>

--- 07-Heroes-SPA/HeroesSpa/Test/router/PublicRoute.test.jsx
+++ 07-Heroes-SPA/HeroesSpa/Test/router/PublicRoute.test.jsx
@@ -1,12 +1,14 @@
 
-import {render,screen} from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
+import { MemoryRouter, Route, Routes } from 'react-router-dom'
 import { AuthContext } from '../../src/auth'
+import { PrivateRoute } from '../../src/router/PrivateRoute'
 import { PublicRoute } from '../../src/router/PublicRoute'
-describe('Pruebas en <PublicRoute/>', () => { 
-    
-    test('should show the children if i not autenticathed', () => { 
-        
-        const contextValue = {logged: false}
+describe('Pruebas en <PublicRoute/>', () => {
+
+    test('should show the children if i not autenticathed', () => {
+
+        const contextValue = { logged: false }
 
         render(
             <AuthContext.Provider value={contextValue}>
@@ -17,6 +19,29 @@ describe('Pruebas en <PublicRoute/>', () => {
         )
         expect(screen.getByText('Ruta Pública')).toBeTruthy();
 
-     })
+    })
 
- })
+    test('should navigate if is autenticathed', () => {
+
+        const contextValue = {
+            logged: true,
+            user: { id: "ABC", name: "Leandro" }
+        }
+        render(
+            <AuthContext.Provider value={contextValue}>
+                <MemoryRouter initialEntries={['/login']}>
+                    <Routes>
+                        <Route path='login' element={
+                            <PublicRoute>
+                                <h1>Ruta Privada</h1>
+                            </PublicRoute>
+                        } />
+                        <Route path='marvel' element={<h1>Página Marvel</h1>} />
+                    </Routes>
+                </MemoryRouter>
+            </AuthContext.Provider>
+        )
+        expect(screen.getByText('Página Marvel')).toBeTruthy();
+    })
+
+})
