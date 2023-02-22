715b934097088ddefff830df4c783afff9d9a8a9
Leandro Martín Balado Vidal <lm.baladovidal@gmail.com>
2023-02-21 23:24:25 -0300

    se agregaron los componentes de <NoteView/> e <ImageGallery/>

--- 08-Journal-App/src/journal/pages/JournalPage.jsx
+++ 08-Journal-App/src/journal/pages/JournalPage.jsx
@@ -1,11 +1,27 @@
+import IconButton from "@mui/material/IconButton"
+import AddOutlined from "@mui/icons-material/AddOutlined"
 import Typography from "@mui/material/Typography"
 import { JournalLayout } from "../layout/JournalLayout"
-import { NothingSelectedView } from "../views"
+import { NoteView, NothingSelectedView } from "../views"
+import { fontSize } from "@mui/system"
 
 export const JournalPage = () => {
   return (
     <JournalLayout>
       <NothingSelectedView/>  
+      {/* <NoteView/> */}
+      <IconButton 
+        size='large'
+        sx={{
+          color:'white',
+          backgroundColor:'error.main',
+          ':hover':{backgroundColor:'error.main', opacity:0.80},
+          position:'fixed',
+          right:50,
+          bottom:50
+        }} >
+        <AddOutlined sx={{fontSize:30}}/>
+      </IconButton>
     </JournalLayout>
 
   )
