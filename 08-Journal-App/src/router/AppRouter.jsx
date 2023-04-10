3b8cf7b3ca35ba4b728d087f646a54b0c356077c
Leandro Martín Balado Vidal <lm.baladovidal@gmail.com>
2023-04-10 11:20:35 -0300

    se creo el customHook useCheckAuth para quitar del appRouter la logica de la comprobacion de estado

--- 08-Journal-App/src/router/AppRouter.jsx
+++ 08-Journal-App/src/router/AppRouter.jsx
@@ -1,25 +1,12 @@
 import { Routes, Route, Navigate } from 'react-router-dom'
-import { useDispatch, useSelector } from 'react-redux'
-import { firebaseAuth } from '../firebase/config'
-import { onAuthStateChanged } from 'firebase/auth'
+import { useCheckAuth } from '../hooks'
 import { AuthRoutes } from '../auth/routes/AuthRoutes'
 import { JournalRoutes } from '../journal/routes/JournalRoutes'
 import { CheckingAuth } from '../ui/'
-import { useEffect } from 'react'
-import { login, logout } from '../store/auth'
 
 export const AppRouter = () => {
 
-    const {status} = useSelector(state=>state.auth)
-    const dispatch = useDispatch()
-    useEffect(() => {
-      onAuthStateChanged(firebaseAuth,async(user)=>{
-        if(!user)return dispatch(logout())
-        const {uid,email,displayName,photoURL}= user
-        dispatch(login({uid,email,displayName,photoURL}))
-      }) 
-    
-    }, [])
+  const {status} = useCheckAuth();
 
     if (status === 'checking'){
         return <CheckingAuth/>
